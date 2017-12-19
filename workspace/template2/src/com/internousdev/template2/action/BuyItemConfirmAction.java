2f0c5013c993a23af6eb5e777c276db75ef0e81a
ociai-yk <yuka1990.03@gmail.com>
2017-12-19 17:30:41 +0900

    12/19_1

---
+++ workspace/template2/src/com/internousdev/template2/action/BuyItemConfirmAction.java
@@ -0,0 +1,13 @@
+package com.internousdev.template2.action;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.template2.dao.BuyItemCompleteDAO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BuyItemConfirmAction {
+
+}
