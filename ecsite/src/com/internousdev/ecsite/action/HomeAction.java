a14cd12b72688fa0255d8c15d1d840f9b36df98f
ociai-yk <yuka1990.03@gmail.com>
2017-12-22 17:43:37 +0900

    12/22

---
+++ ecsite/src/com/internousdev/ecsite/action/HomeAction.java
@@ -0,0 +1,16 @@
+package com.internousdev.ecsite.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport{
+
+	/**ログインボタンを押すと実行
+	 * ログイン画面に遷移する
+	 *
+	 * @return SUCCESS
+	 * */
+
+	public String execute(){
+		return SUCCESS;
+	}
+}
